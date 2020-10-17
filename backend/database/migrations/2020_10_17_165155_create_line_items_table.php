5bc65d867f2400477dd18002f0d9da6e21ea600f
kasey.culmback <kasey.culmback@aon.com>
2020-10-17 10:02:55 -0700

    set up initial models/migrations

---
+++ backend/database/migrations/2020_10_17_165155_create_line_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLineItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('line_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('shopify_id');
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('order_id')->constrained();
+            $table->integer('quantity');
+            $table->boolean('gift_card');
+            $table->string('price');
+            $table->string('total_discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('line_items');
+    }
+}
