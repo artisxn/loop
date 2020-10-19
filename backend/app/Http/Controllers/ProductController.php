476f7bf6353a6c3cc947ae45a75460038c822825
kasey.culmback <kasey.culmback@aon.com>
2020-10-19 12:00:08 -0700

    add cache example

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Repositories\ProductRepository;
+use Cache;
 use Illuminate\Http\Request;
+use Log;
 
 class ProductController extends Controller
 {
@@ -23,8 +25,14 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         return $this->productRepository->paginate();
+
+        // Example of how some basic caching could be implemented
+        // $page = $request->input('page', 1);
+        // return Cache::remember("products_$page", now()->addMinute(), function () {
+        //     return $this->productRepository->paginate();
+        // });
     }
 }
